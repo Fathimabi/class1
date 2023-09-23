79487669bfb951d4bb1513b126340252cce3e136
Fathimabi <145862207+Fathimabi@users.noreply.github.com>
2023-09-23 22:14:35 +0530

    Create Py_functions.py

---
+++ Py_functions.py
@@ -0,0 +1,15 @@
+Python Keywords and Identifiers
+In this session, you will learn about keywords (reserved words in Python) and identifiers (names given to variables, functions, etc.).
+
+1. Python Keywords
+Keywords in Python
+
+False	break	for	not
+None	class	from	or
+True	continue	global	pass
+__peg_parser__	def	if	raise
+and	del	import	return
+as	elif	in	try
+assert	else	is	while
+async	except	lambda	with
+await	finally	nonlocal	yield
